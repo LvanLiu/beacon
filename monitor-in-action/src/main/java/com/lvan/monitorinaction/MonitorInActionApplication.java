20102f2603a2a293c8d2f1e9b701bc1c1b031f03
Qibin Liu <qibin.liu@transsnet.com>
2022-07-14 22:32:18 +0800

    新增监控demo

---
+++ monitor-in-action/src/main/java/com/lvan/monitorinaction/MonitorInActionApplication.java
@@ -0,0 +1,13 @@
+package com.lvan.monitorinaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonitorInActionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MonitorInActionApplication.class, args);
+    }
+
+}
